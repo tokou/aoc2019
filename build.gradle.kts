4eb2d5b5792dcca0362930975940ee92ccb103b8
Tarek Belkahia <tarek.belkahia@360learning.com>
2019-12-01 06:36:19 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("script-runtime"))
+    implementation(kotlin("stdlib-jdk8"))
+}
